b6168b22bef953c7ac3040da382085ffec1cd8c4
Noha Nabil <46314500+nohaelgendy@users.noreply.github.com>
2023-04-06 13:05:07 +0200

    add Error Handling

--- app.py
+++ app.py
@@ -12,12 +12,15 @@ def index():
     if request.method == "POST":
         return render_template("index.html", data = "data")
 
-    #Init API 
-    api = init_garmin_api(email, password)
+    try:
+        #Init API 
+        # api = init_garmin_api(email, password)
+        # username = api.get_full_name()
+        username = ""
+    except Exception as e:
+        error_message = handle_error(e)
+        return render_template("error.html", message=error_message)
 
-    #TODO: Handle Error!
-
-    username = api.get_full_name()
     return render_template("index.html", username = username)
 
 def init_garmin_api(email, password):
@@ -27,3 +30,17 @@ def init_garmin_api(email, password):
     except: 
         return "Garmin Connect Server is not working!!! Please try later."
     return api
+
+def handle_error(e):
+    if "401" in str(e):
+        return "Invalid email or password."
+    elif "ConnectTimeout" in str(e):
+        return "Garmin Connect Server is not responding. Please try again later."
+    else:
+        return "An unknown error occurred. Please try again later."
+
+
+@app.route("/error")
+def error():
+    message = "This is an error message."
+    return render_template("error.html", message=message)
