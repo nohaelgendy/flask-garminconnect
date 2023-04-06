4202b51d67f7cda9d53070e3eff1c6658381b809
Noha Nabil <46314500+nohaelgendy@users.noreply.github.com>
2023-04-06 12:13:15 +0200

    init garmin api, get username from api

--- app.py
+++ app.py
@@ -1,12 +1,29 @@
 import os
-
 from flask import Flask, redirect, render_template, request, url_for
+from garminconnect import Garmin
 
 app = Flask(__name__)
+
+email = os.getenv("EMAIL")
+password = os.getenv("PASSWORD")
 
 @app.route("/", methods=("GET", "POST"))
 def index():
     if request.method == "POST":
         return render_template("index.html", data = "data")
 
-    return render_template("index.html")
+    #Init API 
+    api = init_garmin_api(email, password)
+
+    #TODO: Handle Error!
+
+    username = api.get_full_name()
+    return render_template("index.html", username = username)
+
+def init_garmin_api(email, password):
+    try:  
+        api = Garmin(email, password)
+        api.login() 
+    except: 
+        return "Garmin Connect Server is not working!!! Please try later."
+    return api
